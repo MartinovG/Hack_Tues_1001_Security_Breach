67882335673d5bb36b49b0b6fe30460f5ffb6ebe
topchetoeu <36534413+TopchetoEU@users.noreply.github.com>
2023-03-11 04:18:08 +0200

    add game engine

--- HackTues/Engine/Game.cs
+++ HackTues/Engine/Game.cs
@@ -1,5 +1,5 @@
 ﻿using HackTues.Controls;
-using OpenTK.Audio.OpenAL;
+using HackTues.Physics;
 using OpenTK.Mathematics;
 
 namespace HackTues.Engine;
@@ -28,20 +28,15 @@ public abstract class Game: IGame {
             return Path.GetFullPath("assets");
         }
     }
-    public Map Map { get; protected set; } = new();
-    protected abstract Vector2 Friction { get; }
+    public PhysicsEngine Physics { get; }
+    public Map Map { get; protected set; }
     public abstract Vector2 CameraPos { get; }
     public abstract Layer PlayerLayer { get; }
     public abstract Hitbox PlayerHitbox { get; }
-    public Vector2 PlayerPosition {
-        get => pos;
-        set => pos = value;
-    }
+    public abstract Vector2 PlayerPosition { get; set; }
     public bool Switch { get; protected set; }
 
-    protected Vector2 pos, vel;
 
-    protected abstract Vector2 GetAcceleration(float detla, IController? controller);
     protected virtual void HitX() { }
     protected virtual void HitY() {  }
 
@@ -59,25 +54,12 @@ public abstract class Game: IGame {
             fg.Render();
         }
     }
-    public virtual void Update(float delta, IController? controller) {
-        var acc = GetAcceleration(delta, controller);
+    public virtual void Update(float delta, IController controller) {
+        Physics.Update(delta, controller);
+    }
 
-        vel += acc * delta;
-        vel *= new Vector2(MathF.Pow(Friction.X, delta), MathF.Pow(Friction.Y, delta));
-
-        pos.X += vel.X * delta;
-        if (Map.CollidesWith(PlayerHitbox + pos)) {
-            pos.X -= vel.X * delta;
-            vel.X = 0;
-            HitX();
-        }
-
-        pos.Y += vel.Y * delta;
-        if (Map.CollidesWith(PlayerHitbox + pos)) {
-            pos.Y -= vel.Y * delta;
-            vel.Y = 0;
-            HitY();
-        }
+    public Game() {
+        Physics = new();
     }
 }
 
@@ -97,9 +79,10 @@ public enum WeaponDirection {
     Down,
 }
 
-public class LobbyGame: Game {
-    private Direction direction = Direction.North;
+public class LobbyPlayer: DynamicCollider {
     private float walkCycle = 0;
+    private Direction direction = Direction.North;
+    private LobbyGame game;
 
     private Layer[] east = {
         new("player-office-east-0", new(0, 0), new(34, 84), new(17, 75)),
@@ -122,12 +105,16 @@ public class LobbyGame: Game {
         new("player-office-south-2", new(0, 0), new(40, 83), new(20, 77)),
     };
 
-    protected override Vector2 Friction { get; } = new(0.000001f, 0.000001f);
-    public override Layer PlayerLayer {
+    public override bool HasGravity => false;
+    public override Vector2 Friction { get; } = new(.000001f, .000001f);
+    public override Hitbox Hitbox { get; } = new(new(-12, -5), new(24, 10));
+    public Layer Layer {
         get {
             int i = (int)walkCycle % 4;
-            if (i == 2) i = 0;
-            if (i == 3) i = 2;
+            if (i == 2)
+                i = 0;
+            if (i == 3)
+                i = 2;
 
             return direction switch {
                 Direction.North => north[i],
@@ -138,14 +125,10 @@ public class LobbyGame: Game {
             };
         }
     }
-    public override Hitbox PlayerHitbox { get; } = new(new(-12, -5), new(24, 10));
-    public override Vector2 CameraPos => PlayerPosition;
-    public ILevelLoader LevelLoader { get; }
 
-    protected override Vector2 GetAcceleration(float detla, IController? controller) {
+    public override void Update(float delta, IController controller) {
         var acc = Vector2.Zero;
-        if (controller == null)
-            return acc;
+        if (controller == null) return;
 
         if (controller.Get(Button.Left)) {
             acc -= Vector2.UnitX;
@@ -167,36 +150,29 @@ public class LobbyGame: Game {
         if (acc.Length > 1)
             acc.Normalize();
         acc *= 1000;
-
-        return acc;
-    }
-
-    public override void Update(float delta, IController? controller) {
-        base.Update(delta, controller);
-
-        if (vel.Length > 1) {
-            walkCycle += delta * 4;
-        }
-        else {
-            walkCycle = 0;
-        }
+        Velocity += acc * delta;
 
         if (controller?.Poll(Button.Shoot) == true) {
-            foreach (var entry in Map.EntryLayers) {
-                if ((pos - entry.Position).Length < 16) {
-                    LevelLoader.LoadMap(entry.Texture[6..]);
+            foreach (var entry in game.Map.EntryLayers) {
+                if ((Position - entry.Position).Length < 16) {
+                    game.LevelLoader.LoadMap(entry.Texture[6..]);
                 }
             }
         }
     }
 
-    public LobbyGame(ILevelLoader lobbyLoader) {
-        LevelLoader = lobbyLoader;
-        Map = Map.Load(new FileStream(Path.Combine(AssetsPath, "maps/spawn"), FileMode.Open));
-        PlayerPosition = Map.Spawn;
+    public LobbyPlayer(LobbyGame game) {
+        this.game = game;
     }
 }
-public class LevelGame: Game {
+public class LevelPlayer: DynamicCollider {
+    private float walkCycle = 0;
+    private float kyoteeTime = 0;
+    private float jumpCooldown = 0;
+    private TurnDirection turn = TurnDirection.Right;
+    private WeaponDirection weapon = WeaponDirection.Middle;
+    private LevelGame game;
+
     private Layer[] left = {
         new("player-map-left-0-0", new(0, 0), new(36, 63), new(19, 61)),
         new("player-map-left-0-1", new(0, 0), new(42, 63), new(22, 61)),
@@ -213,13 +189,11 @@ public class LevelGame: Game {
         new("player-map-right-1-1", new(0, 0), new(42, 67), new(22, 61)),
         new("player-map-right-1-2", new(0, 0), new(42, 67), new(21, 61)),
     };
-    private TurnDirection turn = TurnDirection.Right;
-    private WeaponDirection weapon = WeaponDirection.Middle;
-    private float walkCycle = 0;
 
-    protected override Vector2 Friction { get; } = new(0.000001f, 0.1f);
-    public override Vector2 CameraPos => new(Math.Max(1440 / 4, pos.X), 900 / 4);
-    public override Layer PlayerLayer {
+    public override bool HasGravity => true;
+    public override Vector2 Friction { get; } = new(0.000001f, 0.1f);
+    public override Hitbox Hitbox { get; } = new(new(-16, -64), new(32, 64));
+    public Layer Layer {
         get {
             int i = (int)walkCycle % 2;
 
@@ -230,23 +204,13 @@ public class LevelGame: Game {
             };
         }
     }
-    public override Hitbox PlayerHitbox { get; } = new(new(-16, -64), new(32, 64));
-    public ILevelLoader LevelLoader { get; }
 
-    public void LoadMap(string name) {
-        Map = Map.Load(new FileStream(Path.Combine(AssetsPath, "maps/" + name), FileMode.Open));
+    public override void OnCollisionY(ICollider other) {
+        kyoteeTime = .4f;
     }
-
-    private float kyoteeTime = 0;
-    private float jumpCooldown = 0;
-
-    protected override void HitY() {
-        kyoteeTime = .1f;
-    }
-    protected override Vector2 GetAcceleration(float detla, IController? controller) {
+    public override void Update(float delta, IController controller) {
         var acc = Vector2.Zero;
-        if (controller == null)
-            return acc;
+
         if (controller.Get(Button.Left)) {
             acc -= Vector2.UnitX * 1250;
             turn = TurnDirection.Left;
@@ -256,7 +220,7 @@ public class LevelGame: Game {
             turn = TurnDirection.Right;
         }
         if (controller.Get(Button.Up) && kyoteeTime > 0 && jumpCooldown < 0 && controller.Poll(Button.Up)) {
-            vel.Y = -400;
+            Velocity = new(Velocity.X, -500);
             kyoteeTime = 0;
             jumpCooldown = .5f;
         }
@@ -276,15 +240,9 @@ public class LevelGame: Game {
             weapon = WeaponDirection.Down;
         }
 
-        acc += Vector2.UnitY * 700;
+        Velocity += acc * delta;
 
-        return acc;
-    }
-
-    public override void Update(float delta, IController? controller) {
-        base.Update(delta, controller);
-
-        if (Math.Abs(vel.X) > 1) {
+        if (Math.Abs(Velocity.X) > 1) {
             walkCycle += delta * 4;
         }
         else {
@@ -295,7 +253,60 @@ public class LevelGame: Game {
         kyoteeTime -= delta;
     }
 
+    public LevelPlayer(LevelGame game) {
+        this.game = game;
+    }
+}
+
+public class LobbyGame: Game {
+    public LobbyPlayer Player { get; }
+    public override Vector2 PlayerPosition {
+        get => Player.Position;
+        set => Player.Position = value;
+    }
+    public override Layer PlayerLayer => Player.Layer;
+    public override Hitbox PlayerHitbox => Player.Hitbox;
+    public override Vector2 CameraPos => Player.Position;
+    public ILevelLoader LevelLoader { get; }
+
+    public override void Update(float delta, IController controller) {
+        base.Update(delta, controller);
+    }
+
+    public LobbyGame(ILevelLoader lobbyLoader) {
+        LevelLoader = lobbyLoader;
+        Map = Map.Load(new FileStream(Path.Combine(AssetsPath, "maps/spawn"), FileMode.Open));
+        Player = new(this);
+        PlayerPosition = Map.Spawn;
+        Physics.DynamicColliders.Add(Player);
+        Physics.StaticColliders.Add(Map);
+    }
+}
+public class LevelGame: Game {
+    public LevelPlayer Player { get; }
+    public override Vector2 CameraPos => new(Math.Max(1440 / 4, PlayerPosition.X), 900 / 4);
+    public override Vector2 PlayerPosition {
+        get => Player.Position;
+        set => Player.Position = value;
+    }
+    public override Layer PlayerLayer => Player.Layer;
+    public override Hitbox PlayerHitbox => Player.Hitbox;
+    public ILevelLoader LevelLoader { get; }
+
+    public void LoadMap(string name) {
+        Map = Map.Load(new FileStream(Path.Combine(AssetsPath, "maps/" + name), FileMode.Open));
+        Physics.DynamicColliders.Clear();
+        Physics.StaticColliders.Clear();
+        Physics.DynamicColliders.Add(Player);
+        Physics.StaticColliders.Add(Map);
+    }
+
+    public override void Update(float delta, IController controller) {
+        base.Update(delta, controller);
+    }
+
     public LevelGame(ILevelLoader levelLoader) {
         LevelLoader = levelLoader;
+        Player = new(this);
     }
 }
--- HackTues/Physics/PhysicsEngine.cs
+++ HackTues/Physics/PhysicsEngine.cs
@@ -5,24 +5,25 @@ using OpenTK.Mathematics;
 namespace HackTues.Physics;
 
 public class PhysicsEngine: IPhysicsEngine {
-    public List<DynamicCollider> DynamicColliders => new();
-    public List<ICollider> StaticColliders => new();
+    public List<DynamicCollider> DynamicColliders { get; } = new();
+    public List<ICollider> StaticColliders { get; } = new();
     public void Update(float delta, IController controller) {
-        DynamicColliders.ForEach(c => {
+        foreach (var c in DynamicColliders) {
             c.Update(delta, controller);
             var friction = c.Friction;
+            if (c.HasGravity) c.Velocity += new Vector2(0, 1000) * delta;
             c.Velocity *= new Vector2(MathF.Pow(friction.X, delta), MathF.Pow(friction.Y, delta));
             var x = delta * c.Velocity.X;
             var y = delta * c.Velocity.Y;
             CollidedX(c, x);
             CollidedY(c, y);
-        });
+        }
     }
     private void CollidedX(DynamicCollider collider, float deltaX) {
         collider.Position = new(collider.Position.X + deltaX, collider.Position.Y);
         var collided = false;
         foreach (var s in StaticColliders) {
-            if (s.CollidesWith(collider.Hitbox)) {
+            if (s.CollidesWith(collider.Hitbox + collider.Position)) {
                 collider.OnCollisionX(s);
                 collided = true;
                 break;
@@ -30,7 +31,7 @@ public class PhysicsEngine: IPhysicsEngine {
         }
         foreach (var c in DynamicColliders) {
             if (c == collider) continue;
-            if (collider.Hitbox.CollidesWith(c.Hitbox)) {
+            if (collider.Hitbox.CollidesWith(c.Hitbox + collider.Position)) {
                 collider.OnCollisionX(c);
                 c.OnCollisionX(collider);
                 collided = true;
@@ -46,7 +47,7 @@ public class PhysicsEngine: IPhysicsEngine {
         collider.Position = new(collider.Position.X, collider.Position.Y + deltaY);
         var collided = false;
         foreach (var s in StaticColliders) {
-            if (s.CollidesWith(collider.Hitbox)) {
+            if (s.CollidesWith(collider.Hitbox + collider.Position)) {
                 collider.OnCollisionY(s);
                 collided = true;
                 break;
@@ -55,7 +56,7 @@ public class PhysicsEngine: IPhysicsEngine {
         foreach (var c in DynamicColliders) {
             if (c == collider)
                 continue;
-            if (collider.Hitbox.CollidesWith(c.Hitbox)) {
+            if (collider.Hitbox.CollidesWith(c.Hitbox + collider.Position)) {
                 collider.OnCollisionY(c);
                 c.OnCollisionY(collider);
                 collided = true;
